aad29c553c859f8fede44ed692a07666ee15f1b4
pitrodapiyush <pitrodapiyush@gmail.com>
2019-03-01 15:49:56 +0000

    Labels Updated

--- First_Repo_Script.R
+++ First_Repo_Script.R
@@ -1,3 +1,4 @@
 summary(cars)
-plot(cars$speed, cars$dist, col = "red", type = "b"
+plot(cars$speed, cars$dist, col = "red", type = "b")
 # PLease add xlab, ylab and title
+plot(cars$speed, cars$dist, col = "red", type = "b", sub = "GITHUB First Plot", xlab = "Speed", ylab = "Distance", las=1)
